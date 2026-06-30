10d224e684bfb0a3cdde93e2cc1134eda149babb
exobenwalid-svg <exobenwalid@gmail.com>
2026-06-30 22:07:01 +0100

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
+        disallow: ['/admin/', '/draft/', '/preview/'],
       },
     ],
     sitemap: 'https://www.ailiq.xyz/sitemap.xml',
